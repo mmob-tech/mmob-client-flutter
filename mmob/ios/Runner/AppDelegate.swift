892fe6e6f6a8990b73b9bf2c814ce5022a93a1dc
alexw005 <fswoon2@gmail.com>
2023-09-15 21:33:44 +0800

    ios poc

--- mmob/ios/Runner/AppDelegate.swift
+++ mmob/ios/Runner/AppDelegate.swift
@@ -7,7 +7,28 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      
+    let METHOD_CHANNEL_NAME = "com.client.mmob/methodChannel"
+      
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+          
+    let methodChannel = FlutterMethodChannel(name: METHOD_CHANNEL_NAME, binaryMessenger: controller.binaryMessenger)
+    
+    methodChannel.setMethodCallHandler({(call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        switch call.method {
+        case "getMessage":
+            self.getMessage(result:result)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    })
+      
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    private func getMessage(result: FlutterResult){
+        let message = "HI FROM IOS"
+            
+        result(message)
+    }
 }
