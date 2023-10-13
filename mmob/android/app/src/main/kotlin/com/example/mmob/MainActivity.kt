87ad465b8f89e5b42a379b4a9231a3b603b38edb
alexw005 <fswoon2@gmail.com>
2023-10-13 15:04:12 +0800

    android working with passed params

--- mmob/android/app/src/main/kotlin/com/example/mmob/MainActivity.kt
+++ mmob/android/app/src/main/kotlin/com/example/mmob/MainActivity.kt
@@ -7,7 +7,6 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-import com.example.mmob.MmobClientFlutter
 import java.io.Serializable
 
 class MainActivity: FlutterActivity() {
@@ -17,49 +16,22 @@ class MainActivity: FlutterActivity() {
         super.configureFlutterEngine(flutterEngine)
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channel).setMethodCallHandler{
             call, result->
-            if (call.method== "getMessage"){
-                val message=getMessage( argument = call.arguments)
-
-                if(message !=null){
-                    result.success(message)
-                }
-                else{
-                    result.error("Unavailable", "message from Kotlin not there", null)
-
-                }
-            }
             if (call.method=="boot"){
                 mmobBoot(call.arguments as? Map<String,Any>, result)
-
-
-
             }
             else{
                 result.notImplemented()
             }
         }
-
-    }
-
-    private fun getMessage(argument: Any?): String? {
-        return argument as String?
     }
     private fun mmobBoot(argument: Map<String, Any>?,result: MethodChannel.Result){
         val intent = Intent(this, MmobClientFlutter::class.java)
-        val customer_info = argument?.get("customer_info") as? Map<String, String>
-        print( customer_info)
+        val userInfo = argument?.get("customer_info") as? Map<String, String>
+        val configuration = argument?.get("integration_configuration") as? Map<String,String>
         val bundle = Bundle()
-        bundle.putSerializable("customer_info", customer_info as Serializable )
-        intent.putExtra("customer_info",bundle)
+        bundle.putSerializable("customer_info", userInfo as Serializable )
+        bundle.putSerializable("integration_configuration", configuration as Serializable )
+        intent.putExtra("payload",bundle)
         startActivity(intent)
-
-        print(intent)
-
-
     }
-
-//    private fun getMessage( argument ):String{
-//        return "HELLO FROM ANDROID"
-//    }
-
 }
